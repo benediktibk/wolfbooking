e16fdb357b8a9332bb4865502f35998929ac2c89
Benedikt Schmidt <benedikt.schmidt@tum.de>
2015-12-25 22:54:59 +0100

    simplified creation of backend objects

---
+++ Backend/Factory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using Backend.Business;
+using Backend.Persistence;
+
+namespace Backend
+{
+    public class Factory
+    {
+        private string _databaseConnectionString;
+        private BreadRepository _breadRepository;
+        private BreadFactory _breadFactory;
+
+        public string DatabaseConnectionString
+        {
+            get
+            {
+                if (_databaseConnectionString == null)
+                {
+                    var hostName = Environment.MachineName;
+                    _databaseConnectionString = ConfigurationManager.ConnectionStrings[hostName].ConnectionString;
+                }
+
+                return _databaseConnectionString;
+            }
+        }
+
+        public BreadRepository BreadRepository
+        {
+            get
+            {
+                if (_breadRepository == null)        
+                    _breadRepository = new BreadRepository(DatabaseConnectionString);
+
+                return _breadRepository;
+            }
+        }
+
+        public BreadFactory BreadFactory
+        {
+            get
+            {
+                if (_breadFactory == null)
+                    _breadFactory = new BreadFactory(BreadRepository);
+
+                return _breadFactory;
+            }
+        }
+    }
+}
--- Facade/Controllers/BreadsController.cs
+++ Facade/Controllers/BreadsController.cs
@@ -1,31 +1,22 @@
 ﻿using System.Web.Http;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Configuration;
-using System.Net.Http;
 using Backend.Business;
 using Backend.Persistence;
 using System.Net;
+using Backend;
 
 namespace Facade.Controllers
 {
     public class BreadsController : ApiController
     {
+        private static Factory _factory;
         private static BreadFactory _breadFactory;
-        private static BreadRepository _breadRepository;
 
         static BreadsController()
         {
-            var databaseConnectionString = GetDatabaseConnectionString();
-            _breadRepository = new BreadRepository(databaseConnectionString);
-            _breadFactory = new BreadFactory(_breadRepository);
-        }
-
-        private static string GetDatabaseConnectionString()
-        {
-            var hostName = Environment.MachineName;
-            return ConfigurationManager.ConnectionStrings[hostName].ConnectionString;
+            _factory = new Factory();
+            _breadFactory = _factory.BreadFactory;
         }
 
         public IEnumerable<Models.Bread> GetCurrentAvailableBreads()
