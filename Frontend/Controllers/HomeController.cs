8e8af2f5a7add72329fc6393b0f201991311e78d
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-01-02 13:33:36 +0100

    added homecontroller and -view

---
+++ Frontend/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace Facade.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public HomeController()
+        {
+
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            response.Content = new StringContent("<html><body>blob</body></html>");
+            return response;
+        }
+
+        public HttpResponseMessage GetIndex()
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            response.Content = new StringContent("<html><body>blub</body></html>");
+            return response;
+        }
+    }
+}
