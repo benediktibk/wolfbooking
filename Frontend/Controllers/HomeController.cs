70b620da71c5d8b09ef3e41c1eb006b0e8adf69f
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-01-02 14:45:32 +0100

    moved views into separate foldes

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Net.Http;
+﻿using RazorEngine;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 
 namespace Facade.Controllers
@@ -13,16 +17,18 @@ namespace Facade.Controllers
         [Route("home")]
         public HttpResponseMessage Get()
         {
-            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
-            response.Content = new StringContent($"<html><body>blub</body></html>");
-            return response;
+            return GetIndex();
         }
 
         [Route("home/index")]
         public HttpResponseMessage GetIndex()
         {
             var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
-            response.Content = new StringContent($"<html><body>blob</body></html>");
+            var viewPath = HttpContext.Current.Server.MapPath(@"~/Views/Home/Index.cshtml");
+            var template = File.ReadAllText(viewPath);
+            var parsedView = Razor.Parse(template);
+            response.Content = new StringContent(parsedView);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
             return response;
         }
     }
