7a6946f379b1be7417570a651169cf8bfeb3da6d
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-01-29 21:06:35 +0100

    added authorization provider

---
+++ Frontend/Controllers/AuthorizationServerProvider.cs
@@ -0,0 +1,38 @@
+﻿using Backend.Facade;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Frontend.Controllers
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly BookingFacade _bookingFacade;
+
+        public AuthorizationServerProvider(BookingFacade bookingFacade)
+        {
+            _bookingFacade = bookingFacade;
+        }
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            if (!_bookingFacade.IsLoginValid(context.UserName, context.Password))
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+            context.Validated(identity);
+        }
+    }
+}
---
+++ Frontend/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Backend;
+using Frontend.Controllers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(Frontend.Startup))]
+
+namespace Frontend
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+        public static string PublicClientId { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new AuthorizationServerProvider(Factory.BookingFacade)
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
