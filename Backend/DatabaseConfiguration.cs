2c31da16bb52fe8e343cf84871a2c0ae53b7c3ab
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-01-30 09:52:40 +0100

    added database configuration

---
+++ Backend/DatabaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Persistence;
+using System.Data.Entity;
+
+namespace Backend
+{
+    public class DatabaseConfiguration : DbConfiguration
+    {
+        public DatabaseConfiguration()
+        {
+            SetContextFactory(typeof(WolfBookingContext), () => { return Factory.WolfBookingContextFactory.Create(); });
+        }
+    }
+}
