80c1a1571efb134a6f77240daf1467e18e8c294b
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-04 17:39:02 +0200

    made names and descriptions not mandatory in the database

--- Backend/Persistence/Bread.cs
+++ Backend/Persistence/Bread.cs
@@ -10,7 +10,6 @@ namespace Backend.Persistence
 
         [Key]
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
         [Required]
         public decimal Price { get; set; }
--- Backend/Persistence/Room.cs
+++ Backend/Persistence/Room.cs
@@ -10,9 +10,7 @@ namespace Backend.Persistence
 
         [Key]
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Description { get; set; }
         [Required]
         public DateTime Deleted { get; set; }
